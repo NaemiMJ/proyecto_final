6142e5657553d166263a1b6c7c397c53f41fdc9c
Naemi <montse0810jm@gmail.com>
2025-06-29 16:45:52 -0400

    Actualización pequeña en server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware global
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'], // <-- ACEPTA AMBOS
+  origin: (origin, callback) => {
+    callback(null, true); // acepta cualquier origen
+  },
   credentials: true
 }));
 app.use(express.json());
